0f4a2119e240d88c0c783e589f39f381231d1d8d
Thi Khanh Uyen Tran <traty145@mymail.unisa.edu.au>
2021-07-24 16:25:29 +0930

    Implement Double Linked List

--- src/unisa/dse/a1/students/DSEList.java
+++ src/unisa/dse/a1/students/DSEList.java
@@ -8,69 +8,304 @@ import unisa.dse.a1.interfaces.List;
  */
 public class DSEList implements List {
 	
-	public Node head;
-	private Node tail;
+	public Node head, tail = null;
+	private int size = 0;
+
 
 	public DSEList() {
-		
+		head = tail = null;
+		size = 0;
 	}
 	public DSEList(Node head_) {
+		//check if head_ node is not null
+		if(head_ != null) {
+			head_.prev = null;
+			head = head_; 
+			size = 1;
+			
+			if(head_.next == null) {	
+			tail = head;
+			}
+			
+			while(head_.next != null) {			
+				size ++;
+				head_ = head_.next;
+			}
+			tail = head_;
+			
+		}
+		//if head_ is null, we have an empty list
+		else {
+			head = tail = null;
+			size = 0;
+			}
+		
 	}
 	
 	//Takes a list then adds each element into a new list
 	public DSEList(DSEList other) { // Copy constructor. 
+		//check if other list is empty
+		if (other.size == 0) {
+			head = tail = null; //create empty list
+			size = 0;
+		}
+		//add new nodes which has data of other list's nodes into the new list
+		else {
+		Node noderef = other.head;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+		for (int i = 0; i < other.size; i ++) {
+			add(other.get(i));
+			noderef = noderef.next;		
+		}
+		}
+		
 	}
 
 	//remove the String at the parameter's index
 	public String remove(int index) {
+		if(index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		//remove at the list beginning
+		if(index == 0) {
+			Node h  = head;
+			//set new head ater removing old head if list size > 1
+			if(head.next!=null) {
+			head = head.next;
+			head.prev = null;
+			}
+			size--;
+			return h.getString();
+		}
+		else
+			//remove at the end of list
+			if(index == size-1) {
+				Node t = tail;
+				//set new tail 
+				tail = tail.prev;
+				//unlink old tail
+				tail.next = null;
+				size--;
+				return t.getString();
+				}
+			else
+			{
+				Node ref = head;
+				//loop through list to find index
+				for(int i = 0; i < index; i++) {
+					ref = ref.next;
+					}
+				
+				ref.prev.next = ref.next;
+				ref.next.prev = ref.prev;
+				size --;
+		
+				return ref.getString();}
 
 	}
 
 	//returns the index of the String parameter 
 	public int indexOf(String obj) {
+		//check if obj is null
+		if(obj.equals(null)) {
+			 return -1;
+			 }	 
+		    Node current = head; 
+		    int index = -1;
+
+		    for(int i = 0; i < size; i++){
+		        if(current.getString().equals(obj)){
+		             index = i;
+		             }
+		        current = current.next; 
+		        }
+		    return index;
 	}
 	
 	//returns String at parameter's index
 	public String get(int index) {
+		//check if index is out of bound
+		if (index < 0 || index >= size) {
+			return null;
+		}
+		
+		//get head data
+		if(index == 0)
+		{
+			return head.getString();
+		}
+		//get tail data
+		if(index == size-1) 
+		{
+			return tail.getString();
+		}
+		//get data of a node in the middle of list
+		Node node = head;
+		  for (int i=0; i<index; i++) {
+		    node = node.next;
+		  }
+		  return node.getString();		
+	
 	}
 
 	//checks if there is a list
 	public boolean isEmpty() {
+		return size() == 0;
 	}
 
 	//return the size of the list
 	public int size() {
+		return size;
 	}
 	
 	//Take each element of the list a writes them to a string 
 	@Override
 	public String toString() {
+		Node nodeRef = head;
+		  StringBuilder result = new StringBuilder();
+		  //loop through list and print out node data as String type
+		  while (nodeRef != null) {
+		    result.append(nodeRef.getString());
+		    //print out " " between each String but not after the last String
+		    if (nodeRef.next != null) {
+		      result.append(" ");
+		    }
+		    nodeRef = nodeRef.next;
+		  }
+		  return result.toString();   
 	}
 
 	//add the parameter String at of the end of the list
 	public boolean add(String obj) {
+		//check null string
+		if (obj == null){
+			return false;
+			}
+		
+		
+		Node temp = new Node(null, null, obj);
+		// add to empty list
+		if (size == 0) {
+		head = temp;
+		tail = head;
+		
+		}
+		else {
+		//set new tail by creating links to temp node
+		tail.next = temp;
+		temp.prev = tail;
+		tail = temp;	
+		}
+		
+		size++;
+		return true;
+		
 	}
 
 	//add String at parameter's index
 	public boolean add(int index, String obj) {
+		//check if index is out of bound
+		if (index < 0 || index > size ) {
+			throw new IndexOutOfBoundsException();
+			}
+		//check if obj is null
+		if (obj == null){
+			return false;}
+		//create new node with obj data		
+			Node newNode = new Node(null,null,obj);
+			//refer to head node
+			Node n = head;	
+			
+			if(index == size) {
+				//add new node to the end of list
+				add(obj); 
+			}
+			
+			else {
+			if(index == 0) {
+				//add new node to the list beginning
+				n.prev = newNode;
+				newNode.next = n;
+				head = newNode; 
+			}
+			
+			else {
+			//loop through list to find the position to add new node	
+				  for (int i=0; i<index; i++) {
+				    n = n.next;
+				  }
+			
+			newNode.next = n;
+			newNode.prev = n.prev;
+			n.prev.next = newNode;
+			n.prev = newNode;
+			}
+			size++;		
+			}
+			return true;
 	}
 
 	//searches list for parameter's String return true if found
 	public boolean contains(String obj) {
+		return indexOf(obj) != -1;
 	}
 
-	//removes the parameter's String form the list
+	//removes the parameter's String from the list
 	public boolean remove(String obj) {
+		//check if the parameter is null
+		if(obj == null) {
+			throw new NullPointerException();			
+		}
+		if(contains(obj) == false) {
+			return false;
+		}
+		//check if the list is empty
+		if(size == 0) {
+			return false;
+		}
+		else
+		//compare head node data to obj
+		if(head.getString().equals(obj)) {
+				head = head.next;
+				head.prev = null;
+				size--;
+				return true;
+			}	
+		else
+		{
+		Node n = head;
+		//loop through list to find obj string
+		while(n.next!=null && !n.next.getString().equals(obj)) {
+			n = n.next;
+		}
+		//if tail's data equal obj we remove the current tail and set new tail for the list
+		if(n.next.next == null && n.next.getString().equals(obj)) {
+					tail = tail.prev;
+					tail.next = null;
+					size--;
+					return true;}
+		else {
+			//remove a middle node that got obj data
+			n.next = n.next.next;
+			n.next.prev = n;
+			size--;
+			return true;
+		}
+			
+		}
 	}
 	
 	@Override
 	public int hashCode() {
-		return 0;
+		return this.hashCode();
 	}
 
 	@Override
 	public boolean equals(Object other) {
+		if(this.toString().equals(other.toString())) {
 		return true;
+		}
+		else
+		return false;
 	}
 	
 }
--- src/unisa/dse/a1/students/DSEListGeneric.java
+++ src/unisa/dse/a1/students/DSEListGeneric.java
@@ -4,73 +4,286 @@ import unisa.dse.a1.interfaces.ListGeneric;
 
 /**
  * @author simont
+ * @param <T>
  *
  */
-public class DSEListGeneric implements ListGeneric {
+public class DSEListGeneric<T> implements ListGeneric<T> {
 	
-	public NodeGeneric head;
-	private NodeGeneric tail;
+	public NodeGeneric<T> head = null;
+	private NodeGeneric<T> tail = null;
+	private int size = 0;
 
 	public DSEListGeneric() {
-		
+		head = tail = null;
+		size = 0;	
 	}
-	public DSEListGeneric(NodeGeneric head_) {
+	public DSEListGeneric(NodeGeneric<T> head_) {
+		if(head_ != null) {
+			head = head_; //refer list's head to head_ node
+			NodeGeneric<T> n = head;	
+			n.prev = null;
+			size = 1;
+			
+			if(n.next == null) {	
+			tail = head;
+			}
+			
+			while(n.next != null) {			
+				size ++;
+				n = n.next;
+			}
+			tail = n;
+			}
+		else {
+			head = null;
+			tail = null;
+			size = 0;
+			}
 	}
 	
 	//Takes a list then adds each element into a new list
-	public DSEListGeneric(DSEList other) { // Copy constructor. 
+	public DSEListGeneric(DSEListGeneric<T> other) { 
+		//check if other list is empty
+				if (other.size == 0) {
+					head = tail = null; //create empty list
+					size = 0;
+				}
+				
+				NodeGeneric<T> noderef = other.head;
+				for (int i = 0; i < other.size; i ++) {
+					add(other.get(i));
+					noderef = noderef.next;
+				}
 	}
-
-	//remove the String at the parameter's index
-	public String remove(int index) {
+	public boolean add(int index, T obj) {
+		if (index < 0 || index > size) {
+			throw new IndexOutOfBoundsException(Integer.toString(index));
+			}
+		if (obj == null){
+			return false;
+			}
+				
+		NodeGeneric<T> newNode = new NodeGeneric<T>(null,null,obj);
+		NodeGeneric<T> n = head;	
+			if(index == size) {
+				add(obj);
+			}
+			else {
+			if(index == 0) {
+				n.prev = newNode;
+				newNode.next = n;
+				head = newNode;
+			}
+			
+			else {
+				NodeGeneric<T> node = head;
+				  for (int i=0; i<index; i++) {
+				    node = node.next;
+				  }
+			
+			newNode.next = node;
+			newNode.prev = node.prev;
+			node.prev.next = newNode;
+			node.prev = newNode;}
+			size++;		
+			}
+			return true;
+	}
+	public boolean contains(T obj) {
+		return indexOf(obj) != -1;
 
 	}
-
-	//returns the index of the String parameter 
-	public int indexOf(String obj) {
+	public boolean remove(T obj) {
+		//check if the parameter is null
+				if(obj == null) {
+					throw new NullPointerException();			
+				}
+				//check if the list is empty
+				if(isEmpty()) {
+					return false;
+				}
+				else
+				//compare head node data to obj
+				if(head.get().equals(obj)) {
+					head = head.next;
+					head.prev = null;
+					size--;
+					return true;
+					}
+				else {
+					NodeGeneric<T> n = head;
+					
+					while(n.next!=null && !n.next.get().equals(obj)) {
+						n = n.next;
+					}
+					if(n.next.next == null && n.next.get().equals(obj)) {
+								tail = tail.prev;
+								tail.next = null;
+								size--;
+								return true;}
+					else {
+						n.next = n.next.next;
+						n.next.prev = n;
+						size--;
+						return true;
+					}	
+				}		
 	}
+	public T remove(int index) {
+		if(index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		if(size == 0) {
+			return null;
+		}
+		if(index == 0) {
+			NodeGeneric<T> h  = head;
+			head = h.next;
+			size--;
+			return h.get();
+		}
+		if(size == 1) {
+			tail = head = null;
+			size = 0;
+			return head.get();
+		}
+		
+		if(index == size-1) {
+			NodeGeneric<T> t = tail;
+			tail = t.prev;
+			tail.next = null;
+			size--;
+			return t.get();
+		}
+
+		NodeGeneric<T> ref = head;
+		for(int i = 0; i < index; i++) {
+			ref = ref.next;
+		}
+		ref.prev.next = ref.next;
+		ref.next.prev = ref.prev;
+		size --;
+		
+		return ref.get();
+	}
+	public T get(int index) {
+		if (index < 0 || index >= size) {
+			return null;
+		}
+		if (head == null) {
+			return null;
+		}
+		if(index == 0)
+		{
+			return head.get();
+		}
+		if(index == size-1) {
+			return tail.get();
+		}
+		NodeGeneric<T> node = head;
+		  for (int i=0; i<index; i++) {
+		    node = node.next;
+		  }
+		  return node.get();		
 	
-	//returns String at parameter's index
-	public String get(int index) {
 	}
+	public int indexOf(T node) {
+		if(node == null) {
+			 throw new NullPointerException();
+		 }	 
+		 
+		NodeGeneric<T> current = head;
+		    int index = -1;
 
-	//checks if there is a list
-	public boolean isEmpty() {
+		    for(int i = 0; i < size; i++){
+		        if(current.get().equals(node)){
+		             index = i;
+		             return index;
+		        }
+		        current = current.next; 
+		    }
+		    return index;
 	}
-
-	//return the size of the list
-	public int size() {
-	}
-	
-	//Take each element of the list a writes them to a string 
-	@Override
-	public String toString() {
-	}
-
-	//add the parameter String at of the end of the list
-	public boolean add(String obj) {
-	}
-
-	//add String at parameter's index
-	public boolean add(int index, String obj) {
-	}
-
-	//searches list for parameter's String return true if found
-	public boolean contains(String obj) {
-	}
-
-	//removes the parameter's String form the list
-	public boolean remove(String obj) {
-	}
-	
-	@Override
-	public int hashCode() {
-		return 0;
-	}
-
-	@Override
-	public boolean equals(Object other) {
+	public boolean add(T obj) {
+		if (obj == null){
+			return false;}
+				
+		NodeGeneric<T> temp = new NodeGeneric<T>(null, null, obj);
+		// add to empty list
+		if (size == 0) {
+		tail = head = temp;
+		size = 1;
+		return true;
+		}
+		//add to list that has one node
+		if (size == 1){
+		head.next = temp;
+		temp.prev = head;
+		temp.next = null;
+		tail = temp; 
+		size ++;
+		return true;
+		}
+		
+		tail.next = temp;
+		temp.prev = tail;
+		temp.next = null;
+		tail = temp;
+		size++;
 		return true;
 	}
+	public boolean isEmpty() {
+		return size() == 0;
+
+	}
+	public int size() {
+		return size;
+	}
+	@Override
+	public String toString() {
+		NodeGeneric<T> nodeRef = head;
+		StringBuilder result = new StringBuilder();
+		  while (nodeRef != null) {
+		    result.append(nodeRef.get());
+		    if (nodeRef.next != null) {
+		      result.append(" ");
+		    }
+		    nodeRef = nodeRef.next;
+		  }
+		  return result.toString();  
+		
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if(getClass() != obj.getClass()) {
+			return false;
+		}
+		DSEListGeneric<T> ref = (DSEListGeneric<T>) obj;
+		
+		if(this.size != ref.size) {
+			return false;
+		}
+		NodeGeneric<T> temp1 = this.head;
+		NodeGeneric<T> temp2 = ref.head;
+		while(temp1!= null && temp2 != null) {
+			
+			if(temp1.get() != temp2.get()) {
+				return false;}
+				
+				temp1 = temp1.next;
+				temp2 = temp2.next;
+			}
+		return true;
+		
+	}
+	@Override
+	public int hashCode() {
+		return this.hashCode();
+	}
+
 	
 }
